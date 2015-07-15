cba529eeb3d4412b5979e6a4971f30b256b5def6
Jerald Schoudt <jschoudt@us.ibm.com>
2015-07-15 13:27:55 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,3 +1,24 @@
-# personality-insights-twitter-python
+# IBM Watson Personality Insights Twitter Python Sample
 
-A simple python example that calls the twitter API to fetch a user's timeline and then calls Watson Personality Insights to estimate their personality traits.
+This sample shows how to get Twitter data using the [Twitter REST API](https://dev.twitter.com/rest/public) 
+(via the [python-twitter client library](https://github.com/bear/python-twitter)) and submit it to the 
+(Personality Insights Service)[https://www.ibm.com/smarterplanet/us/en/ibmwatson/developercloud/personality-insights.html].
+
+For non-twitter samples and more details on how to setup your Personality Insights service in bluemix see the [official 
+Watson Developer Cloud samples](https://github.com/watson-developer-cloud).
+
+## Setup
+
+This sample was developed on Python 2.6.6 with the python-twitter and requests libraries, installed via pip.
+
+## Configuring your Twitter and Personality Insights Credentials
+
+To configure the sample, copy config.py.example to config.py 
+and fill in your Twitter and Personality Insights Credentials. Instructions are provided in the config.py.example file.
+
+## Running the sample
+
+Provide a twitter handle/screen name WITHOUT the leading @ sign.  Example:
+
+    python twitteranalyzer.py jschoudt
+
---
+++ twitteranalyzer.py
@@ -0,0 +1,46 @@
+import sys
+import requests
+import json
+import twitter
+import config
+
+def convert_status_to_pi_content_item(s):
+    # My code here
+    return { 
+        'userid': str(s.user.id), 
+        'id': str(s.id), 
+        'sourceid': 'python-twitter', 
+        'contenttype': 'text/plain', 
+        'language': s.lang, 
+        'content': s.text, 
+        'created': 1000 * s.GetCreatedAtInSeconds(),
+        'reply': (s.in_reply_to_status_id == None),
+        'forward': False
+    }
+    
+
+handle = sys.argv[1]
+
+twitter_api = twitter.Api(consumer_key=config.twitter_consumer_key,
+                  consumer_secret=config.twitter_consumer_secret,
+                  access_token_key=config.twitter_access_token,
+                  access_token_secret=config.twitter_access_secret,
+                  debugHTTP=True)
+
+statuses = twitter_api.GetUserTimeline(screen_name=handle,
+                  count=200,
+                  include_rts=False)
+
+pi_content_items_array = map(convert_status_to_pi_content_item, statuses)
+pi_content_items = { 'contentItems' : pi_content_items_array }
+
+r = requests.post(config.pi_url + '/v2/profile', 
+			auth=(config.pi_username, config.pi_password),
+			headers = {
+                'content-type': 'application/json',
+                'accept': 'application/json'
+            },
+			data=json.dumps(pi_content_items)
+		)
+print("Profile Request sent. Status code: %d, content-type: %s" % (r.status_code, r.headers['content-type']))
+print json.loads(r.text)
